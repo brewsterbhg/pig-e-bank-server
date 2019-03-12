d4cc3c12b8b9b197bee843ce2f301ff76d908be7
Keith Brewster <keith.brewster22@gmail.com>
2019-03-12 11:12:15 -0400

    Export server module for testing. Add compression. Add supertest

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const compression = require("compression");
 const PORT = process.env.PORT_NUMBER || 3000;
+const routes = require("./api");
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(compression());
+app.use("/v1", routes);
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+module.exports = app;

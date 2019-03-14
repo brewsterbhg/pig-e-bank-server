e4ac5782dba44e337f75a1f72c130903b6552ce0
Sandes de Silva <sdesilva@tribalscale.com>
2019-03-14 12:11:08 -0400

    recreate database file when app starts
    Co-authored-by: Rahul Verma <rverma@tribalscale.com>

--- db/createDb.js
+++ db/createDb.js
@@ -4,8 +4,6 @@ const fs = require('fs')
 const knex = require('knex')({
   client: 'sqlite3',
   connection: {
-    // filename: 'file:memDb1?mode=memory&cache=shared'
-    // filename: ':memory:'
     filename: './sqlite.db'
   },
   useNullAsDefault: true
@@ -13,8 +11,10 @@ const knex = require('knex')({
 
 const checkDbFile = path => fs.existsSync(path)
 
-if (!checkDbFile('./sqlite.db')) {
-  generateSchema(knex).then(() => process.exit(0))
+if (checkDbFile('./sqlite.db')) {
+  console.log('previous db exists...deleting')
+  fs.unlinkSync('./sqlite.db')
 }
+generateSchema(knex).then(() => process.exit(0))
 
 console.log('db created')
